9aa3fdca7087cd7919e8ac1376d2c45b4a53b3cf
Anton Korepanov <3bepb2002@gmail.com>
2025-07-29 17:36:06 +0300

    update hw_dag.py and create README.md

--- dags/hw_dag.py
+++ dags/hw_dag.py
@@ -5,7 +5,9 @@ import sys
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
 
-path = os.path.expanduser('~/airflow_hw')
+
+
+path = '/opt/airflow'
 # Добавим путь к коду проекта в переменную окружения, чтобы он был доступен python-процессу
 os.environ['PROJECT_PATH'] = path
 # Добавим путь к коду проекта в $PATH, чтобы импортировать функции
